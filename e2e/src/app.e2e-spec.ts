d2e61c2eb1ddfbe1b793507578df8b0e1c39cede
DESKTOP-4KMOG4Q\Angel <angelotebh@hotmail.com>
2019-02-20 23:53:59 +0100

    Adds e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
 describe('new App', () => {
   let page: AppPage;
@@ -7,8 +8,25 @@ describe('new App', () => {
     page = new AppPage();
   });
 
-  it('should be blank', () => {
+  it('should allow to add an item', async () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toContain('The world is your oyster.');
+
+    await page.waitForAddBtn();
+    // await browser.driver.sleep(1000);
+    await page.getAddBtn().click();
+     await browser.driver.sleep(5000);
+    // await browser.driver.sleep(1000);
+    // const itemName = 'test item';
+    // await page.getNameField().sendKeys(itemName);
+    // await page.getImportantField().click();
+    // await page.getRemarksField().sendKeys('this is a remark');
+    // await page.getOkBtn().click();
+    // await browser.driver.sleep(1000);
+    // const items = await page.getItems();
+    // console.log(items[0]);
+    // expect(items.length).toEqual(1);
+    // expect(items.first.findElement(by.css('ion-label')).getText()).toEqual(itemName + ' (+info)');
+
+
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,45 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, protractor } from 'protractor';
+// import * as $ from "jquery"
 
 export class AppPage {
+
+  addButton = '#addBtn';
+
+
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.deepCss('app-root ion-content')).getText();
+  getElementById(id: string) {
+    return element(by.id(id));
+  }
+
+  getAddBtn() {
+    return element(by.id('addBtn'));
+  }
+
+  waitForAddBtn() {
+    let addBtn = this.getOkBtn();
+    return browser.wait(protractor.ExpectedConditions.elementToBeClickable(element(by.id('addBtn'))));
+  }
+
+  getNameField() {
+    return element(by.css('#name input'));
+  }
+
+  getRemarksField() {
+    return element(by.css('#remarks textarea'));
+  }
+
+  getImportantField() {
+    return element(by.css('#important'));
+  }
+
+  getOkBtn() {
+    return element(by.id('okBtn'));
+  }
+
+  async getItems() {
+    return element.all(by.css('ion-item'));
   }
 }
