959c462d3aebc38dc84f0a4c2695e506e40c7598
ANGEL\Angel <angelotebh@hotmail.com>
2019-03-16 09:41:39 +0100

    Changes item page buttons position

--- src/app/item/item.page.spec.ts
+++ src/app/item/item.page.spec.ts
@@ -57,8 +57,8 @@ describe('ItemPage', () => {
     return getPageElement().querySelector('ion-header ion-toolbar ion-title').textContent;
   }
 
-  function getButtonText(): string {
-    return getPageElement().querySelectorAll('ion-footer ion-buttons ion-button')[1].textContent;
+  function getFirstButtonText(): string {
+    return getPageElement().querySelectorAll('ion-footer ion-buttons ion-button')[0].textContent;
   }
 
   function getNameInputValue(): string {
@@ -99,7 +99,7 @@ describe('ItemPage', () => {
     createComponentWithExistingItem();
     expect(component).toBeTruthy();
     expect(getTitleText()).toEqual('Ver artículo');
-    expect(getButtonText()).toEqual('Guardar');
+    expect(getFirstButtonText()).toEqual('Guardar');
     expect(getNameInputValue()).toEqual(existingItem.name);
     expect(getRemarksInputValue()).toEqual(existingItem.remarks);
     expect(getImportantInputValue()).toEqual(existingItem.important);
@@ -110,7 +110,7 @@ describe('ItemPage', () => {
     createComponentWithNewItem();
     expect(component).toBeTruthy();
     expect(getTitleText()).toEqual('Nuevo artículo');
-    expect(getButtonText()).toEqual('Añadir');
+    expect(getFirstButtonText()).toEqual('Añadir');
     expect(getNameInputValue()).toBeNull();
     expect(getRemarksInputValue()).toBeNull();
     expect(getImportantInputValue()).toEqual(false);
